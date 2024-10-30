c2f9b572e461bb8cb857b16ba2bf9c22c1015417
Yaroslav <yaroslawsh04@gmail.com>
2024-10-30 12:48:31 +0100

    Added tailwind

--- README.md
+++
@@ -1,1 +0,0 @@
-# Myru-MyrReact
--- myru-myr/src/App.tsx
+++ myru-myr/src/App.tsx
@@ -1,10 +1,9 @@
- const App = () => {
-
+const App = () => {
   return (
     <>
-     AAA
+      <h1 className="text-xl text-font-bold text-red-500">AAA</h1>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
